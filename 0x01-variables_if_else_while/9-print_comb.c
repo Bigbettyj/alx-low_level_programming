5c054f85d3aa8986502300f9386f689f2bfb1955
Bigbettyj <bigbettyj7@gmail.com>
2022-10-17 13:01:18 -0700

    combine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int j;
 
-	for (j = 48; j <= 58; j++)
+	for (j = 48; j <= 57; j++)
 	{
 		putchar(j);
 		if (j != 57)
+		{
 			putchar(',');
 			putchar(' ');
+		}
 	}
 		putchar('\n');
 	return (0);
