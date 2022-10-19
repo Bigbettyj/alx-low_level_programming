1b4cab20414deb1e0c2ff143c2059bb70988d34d
Bigbettyj <bigbettyj7@gmail.com>
2022-10-19 05:28:30 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c: character
+ * Return: 1 if character is alphabet, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (0);
+	else
+		return (0);
+}
