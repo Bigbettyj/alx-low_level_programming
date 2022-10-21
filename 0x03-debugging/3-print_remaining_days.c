e14c99944a0746e3cbbb7a34f9b35a00d5b2dfdc
Bigbettyj <bigbettyj7@gmail.com>
2022-10-21 14:22:59 -0700

    leapyear

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -10,7 +9,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+	if ((year % 4 == 0 || year % 400 == 0 ) && !(year % 100 == 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
