fa97ebb6399f97ccd27b38ee41612528c3b1a012
Bigbettyj <bigbettyj7@gmail.com>
2022-10-24 15:18:51 -0700

    rvstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ * Return: Always 0 (success)
+ */
+void rev_string(char *s)
+{
+	char rv s[0];
+	int d = 0;
+	int k;
+
+	while (s[d] != '\0')
+		d++;
+	for (k = 0; k < d; k++)
+	{
+		c--;
+		rv = s[k];
+		s[k] = s[d];
+		s[d] = rv;
+	}
+}
