4588e9a6a62249070a6a1e37f699e47cf82087ab
Bigbettyj <bigbettyj7@gmail.com>
2022-10-27 05:12:26 -0700

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalize every word of a string
+ * @str: string
+ * Return: pointer to string
+ */
+char *cap_string(char *str)
+{
+	int b = 0;
+
+	while (str[b])
+	{
+		while (!(str[b] >= 'a' && str[b] <= 'z'))
+			b++;
+		if (str[b - 1] == ' ' ||
+				str[b - 1] == ',' ||
+				str[b - 1] == ';' ||
+				str[b - 1] == '.' ||
+				str[b - 1] == '!' ||
+				str[b - 1] == '?' ||
+				str[b - 1] == '\n' ||
+				str[b - 1] == '\t' ||
+				str[b - 1] == '"' ||
+				str[b - 1] == '{' ||
+				str[b - 1] == '}' ||
+				str[b - 1] == '(' ||
+				str[b - 1] == ')' ||
+				b == 0)
+			str[b] -= 32;
+		b++;
+	}
+	return (str);
+}
