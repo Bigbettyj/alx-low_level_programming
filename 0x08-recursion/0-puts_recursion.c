75850f3eb828190e2afe60a1caca809c5d50e702
Bigbettyj <bigbettyj7@gmail.com>
2022-11-02 09:52:59 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: string pointer
+ * Return: Always 0 (success)
+ */
+void _puts_recursion(char *s)
+{
+	int a;
+
+	a = 0
+	if (s[a] != '\0')
+	{
+		return (s[a]);
+	}
+	a++;
+	_putchar('\n');
+}
