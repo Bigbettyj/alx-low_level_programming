a35251fdb490bef46db0bf540bbd2a1e8d9ebc88
Bigbettyj <bigbettyj7@gmail.com>
2022-11-03 10:17:09 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,13 +13,31 @@ int _strlen_recursion(char *s)
 	return (1 + _strlen_recursion(++s));
 }
 /**
+ * checkpan - palindrome
+ * @s: pointer
+ * @r: position
+ * Return: result
+ */
+int checkpan(char *s, int r)
+{
+	if (r < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + r))
+	{
+		return (checkpan(s + 1, r - 2));
+	}
+	return (0);
+}
+/**
  * is_palindrome - checks if string is empty
  * @s: string
  * Return: returns 1 if a string is a palindrome and 0 if not
  */
 int is_palindrome(char *s)
 {
-	if (s < 0)
-		return (1);
-	else
+	int len = _strlen_recursion(s);
 
+	return (checkpan(s, len - 1));
+}
