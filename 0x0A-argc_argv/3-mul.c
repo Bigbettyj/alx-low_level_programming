bbca718edd4e659d988f483e9ff13795b131d56b
Bigbettyj <bigbettyj7@gmail.com>
2022-11-04 14:54:40 -0700

    mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
 {
 	int n, m;
 
-	n = atoi(argv[1]);
-	m = atoi(argv[2]);
 	if (argc == 3)
 	{
+		n = atoi(argv[1]);
+		m = atoi(argv[2]);
 		printf("%d\n", m * n);
 		return (0);
 	}
