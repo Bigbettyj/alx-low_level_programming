d590eb120f91aece19d61459eca0019913be175c
Bigbettyj <bigbettyj7@gmail.com>
2022-11-04 13:25:42 -0700

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
