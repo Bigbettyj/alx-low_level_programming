270b40350eb7e76c6af813d91d130fb861258ae5
Bigbettyj <bigbettyj7@gmail.com>
2022-11-10 13:12:04 -0800

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,39 +7,32 @@
  * @new_size: size of new memory
  * Return: new pointer
  */
-(m);
-}
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *m;
 	char *n, *r;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
+	if (ptr != NULL)
+		n = ptr;
 	{
-		m = (malloc(new_size));
-		if (m == NULL)
-			return (NULL);
-		return (m);
+		(malloc(new_size));
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	r = ptr;
-	m = malloc(sizeof(*r) * new_size);
-
-	if (m == NULL)
+	r = malloc(new_size);
+	if (r == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
-	n = m;
 	for (i = 0; i < old_size && i < new_size; i++)
-		n[i] = r++;
+	{
+		*(r + i) = n[i];
+	}
 	free(ptr);
-	return (m);
+	return (r);
 }
