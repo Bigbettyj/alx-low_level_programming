188c861813331eb178a0519365542431fca2a29f
Bigbettyj <bigbettyj7@gmail.com>
2022-11-15 08:38:34 -0800

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * main - multiplies two positive numbers
  * @agrc: count arguments
@@ -15,18 +16,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	for (a = 1, a < argc; a++)
+	for (a = 1; a < argc; a++)
 	{
 		for (b = 0; argv[a][b] != '\0'; b++)
 		{
 			if (argv[a][b] > 57 || argv[a][b] < 48)
 			{
-				printf("Error\n")
+				printf("Error\n");
 					exit(98);
 			}
 		}
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
