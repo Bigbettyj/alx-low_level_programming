20b35924a34c88c1b191247f76de5436870df45c
Bigbettyj <bigbettyj7@gmail.com>
2022-11-17 07:19:22 -0800

    sum

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,13 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_str(args, char *);
+		str = va_arg(args, char *);
 		if (str)
 			printf("%s", str);
 		else
 			printf("(nil)");
 		if (i < n - 1 && separator != NULL)
-			printf("%s", separator)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
