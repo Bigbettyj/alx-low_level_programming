30bafb2a02f051da34e286c218acb583e57c1fb3
Bigbettyj <bigbettyj7@gmail.com>
2022-11-17 05:29:25 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,16 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	int m, sum;
 
+	if (n == 0)
+		return (0);
+	
 	va_start(args, n);
 
 	sum = 0;
 	for (m = 0, m < n, m++)
-	{
-		if (n == 0)
-		{
-			return (0);
-		}
-		sum += va_arg(args, const unsigned int);
+		sum += va_arg(args, int);
 
 		va_end(args);
 		return (sum);
-	}
 }
 
