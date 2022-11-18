ab1b10acbbfc7cc65429d7458b3b3fd38b8a2f23
Bigbettyj <bigbettyj7@gmail.com>
2022-11-18 06:37:52 -0800

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its own opcodes
+ * @argc: counts arguments
+ * @argv: vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *arr;
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
