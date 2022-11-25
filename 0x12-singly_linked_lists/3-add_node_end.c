eb36aece0399eb361ce94fac8da97e1e57fe4bdb
Bigbettyj <bigbettyj7@gmail.com>
2022-11-25 06:47:31 -0800

    add end_node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: head of list_t list
+ * @str: string to be copied
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+	size_t cchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup[str];
+
+	for (cchar = 0; str[cchar];)
+		cchar++;
+	new->len = cchar;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (*head);
+}
