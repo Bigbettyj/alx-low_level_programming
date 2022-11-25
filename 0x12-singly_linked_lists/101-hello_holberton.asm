1df38ebcaaee7d560093c11cd0e5a6c896efd4b0
Bigbettyj <bigbettyj7@gmail.com>
2022-11-25 08:16:53 -0800

    hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .text
-global main
 extern printf
+global main
 
 section .data
-msg db "Hello, Holberton" , 0
+msg db 'Hello, Holberton', 0
 fmt db "%s", 10, 0
 
 main:
-mov esi, fmt
-mov edi, msg
-mov eax, 0
+mov esi,fmt
+mov edi,msg
+mov eax,0
 call printf
 
-mov eax, 0
+mov eax,0
 ret
