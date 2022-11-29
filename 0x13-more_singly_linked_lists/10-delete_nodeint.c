0857c98712ae75eff6b22e97b5a9ba1c5d542f2d
Bigbettyj <bigbettyj7@gmail.com>
2022-11-29 04:10:12 -0800

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *h;
 	unsigned int node;
 
-	*h = *head;
+	h = *head;
 	if (h == NULL)
 		return (-1);
 	if (index == 0)
