dd4c9261e565ba0fa5ece9b3b3558590d441902b
Bigbettyj <bigbettyj7@gmail.com>
2022-12-07 10:18:55 -0800

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	letters = 0;
 	if (text_content != NULL)
 	{
-		for (letters = 0; text_content[letters])
+		for (letters = 0; text_content[letters];)
 			letters++;
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
